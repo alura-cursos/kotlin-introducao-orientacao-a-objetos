d4744727c27e42968f06577c27aa5a6afe47a77b
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-05 11:53:27 -0200

    1.4

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,12 @@
 fun main(){
     println("Bem vindo ao Bytebank")
-    val titular = "Alex"
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
 
     println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
 }
