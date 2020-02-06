429a15044507f11c7b3888942181e4d25cb24776
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-06 15:28:49 -0200

    2.2

--- src/main.kt
+++ src/main.kt
@@ -1,19 +1,36 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000
+    var i = 0
+    while(i < 5){
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
 
-    if(saldo > 0.0){
+//    for (i in 5 downTo 1) {
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+
+//    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
         println("conta é positiva")
-    } else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("conta é neutra")
     } else {
         println("conta é negativa")
