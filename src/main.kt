0ba087620fd9cfb61ca228637cf829cc55b9b595
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-06 14:39:54 -0200

    2.1

--- src/main.kt
+++ src/main.kt
@@ -5,8 +5,23 @@ fun main(){
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo -= 1000
 
     println("titular $titular")
     println("número da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if(saldo > 0.0){
+        println("conta é positiva")
+    } else if (saldo == 0.0){
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
