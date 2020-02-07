cc44db04bc98ce43c46ad2411c8062238d281b29
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-07 11:35:00 -0200

    3.1

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,31 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos(){
     var i = 0
     while(i < 5){
         val titular: String = "Alex $i"
@@ -13,18 +39,16 @@ fun main() {
         i++
     }
 
-//    for (i in 5 downTo 1) {
-//        val titular: String = "Alex $i"
-//        val numeroConta: Int = 1000 + i
-//        var saldo = i + 10.0
-//
-//        println("titular $titular")
-//        println("número da conta $numeroConta")
-//        println("saldo da conta $saldo")
-//        println()
-//    }
+    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-//    testaCondicoes(saldo)
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
 }
 
 fun testaCondicoes(saldo: Double) {
