bfc55388736ec098e8656480de2c47a87a60a40a
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-02-21 15:08:57 -0300

    clean: Criação da classe Conta e do arquivo de teste testarContaFake. Readaptação do arquivo main.kt.

--- src/main.kt
+++ src/main.kt
@@ -2,86 +2,11 @@ fun main() {
     println("Bem vindo ao Bytebank!")
 
 
-    val contaAlex = Conta(titular = "Alex", numero = 1000)
-    contaAlex.deposita(200.0)
-
-    val contaFran = Conta(numero = 1001, titular = "Fran")
-    contaFran.deposita(300.0)
-
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.saldo)
-
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.saldo)
-
-    println("depositando na conta do Alex")
-    contaAlex.deposita(50.0)
-    println(contaAlex.saldo)
-
-    println("depositando na conta da Fran")
-    contaFran.deposita(70.0)
-    println(contaFran.saldo)
-
-    println("sacando na conta do Alex")
-    contaAlex.saca(250.0)
-    println(contaAlex.saldo)
-
-    println("sacando na conta da Fran")
-    contaFran.saca(100.0)
-    println(contaFran.saldo)
-
-    println("saque em excesso na conta do Alex")
-    contaAlex.saca(100.0)
-    println(contaAlex.saldo)
-
-    println("saque em excesso na conta da Fran")
-    contaFran.saca(500.0)
-    println(contaFran.saldo)
-
-    println("Transferência da conta da Fran para o Alex")
-
-    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
-        println("Transferência sucedida")
-    } else {
-        println("Falha na transferência")
-    }
-
-    println(contaAlex.saldo)
-    println(contaFran.saldo)
+    testarContaFake()
 
     testarCopiasEReferencias()
     testarLacos()
     testarCondicoes(45.55)
-
 }
 
-class Conta(
-    var titular: String,
-    val numero: Int
-) {
-    var saldo = 0.0
-        private set
 
-    fun deposita(valor: Double) {
-        if (valor > 0) {
-            this.saldo += valor
-        }
-    }
-
-    fun saca(valor: Double) {
-        if (saldo >= valor) {
-            saldo -= valor
-        }
-    }
-
-    fun transfere(valor: Double, destino: Conta): Boolean {
-        if (saldo >= valor) {
-            saldo -= valor
-            destino.deposita(valor)
-            return true
-        }
-        return false
-    }
-}
